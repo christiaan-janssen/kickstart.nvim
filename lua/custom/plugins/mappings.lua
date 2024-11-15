dad16efec47b19cb7457dad67879e41f9cd6a84b
Christiaan Janssen <christiaanjanssen@outlook.com>
2024-11-15 07:55:06 +0100

    fix: Fix tmux mappings

--- lua/custom/plugins/mappings.lua
+++ lua/custom/plugins/mappings.lua
@@ -1,9 +1,11 @@
 -- Setup keymappings here
-
+--
 local map = vim.keymap.set
 
-map('n', '<c-h>', '<cmd>TmuxNavigateLeft<cr>')
-map('n', '<c-j>', '<cmd>TmuxNavigateDown<cr>')
-map('n', '<c-k>', '<cmd>TmuxNavigateUp<cr>')
-map('n', '<c-l>', '<cmd>TmuxNavigateRight<cr>')
-map('n', '<c-\\>', '<cmd>TmuxNavigatePrevious<cr>')
+return {
+  map('n', '<c-h>', '<cmd>TmuxNavigateLeft<cr>'),
+  map('n', '<c-j>', '<cmd>TmuxNavigateDown<cr>'),
+  map('n', '<c-k>', '<cmd>TmuxNavigateUp<cr>'),
+  map('n', '<c-l>', '<cmd>TmuxNavigateRight<cr>'),
+  map('n', '<c-\\>', '<cmd>TmuxNavigatePrevious<cr>'),
+}
